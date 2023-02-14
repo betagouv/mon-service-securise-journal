f59168dc2c0b8483ede4c4a673b380bd552a80c8
Christophe Cadilhac <christophe@malea.dev>
2023-02-14 17:23:38 +0100

    Expose les données non agrégées
    … pour permettre un filtrage dynamique côté Metabase.
    
    Co-authored-by: Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>

--- migrations/20230214151022_creationVueMoyennesIndiceCyber.js
+++ migrations/20230214151022_creationVueMoyennesIndiceCyber.js
@@ -1,6 +1,5 @@
 const vue = `journal_mss.vue_moyennes_indice_cyber`;
 
-
 exports.up = knex => knex.raw(`CREATE OR REPLACE VIEW ${vue} AS 
 
 WITH
@@ -13,16 +12,16 @@ WITH
     details_indice_cyber AS (
         SELECT id AS id_evenement_indice_cyber,
                details."categorie",
-               details."indice"
+               details."indice",
+               ((donnees->>'nombreMesuresCompletes')::FLOAT / (donnees->>'nombreTotalMesures')::FLOAT) * 100 AS completion
         FROM journal_mss.vue_evenements_sans_services_supprimes,
              jsonb_to_recordset(donnees -> 'detailIndiceCyber') as details("categorie" text, "indice" float)
         WHERE type = 'COMPLETUDE_SERVICE_MODIFIEE'
     )
-SELECT categorie, avg(indice) AS moyenne
+SELECT categorie, indice, completion
 FROM evenements_plus_recents
 JOIN details_indice_cyber ON id_dernier_evenement = id_evenement_indice_cyber
-GROUP BY categorie
-    
+   
 ;`);
 
-exports.down = knex => knex.raw(`DROP VIEW IF EXISTS ${vue}`);
+exports.down = knex => knex.raw(`DROP VIEW IF EXISTS ${vue};`);
