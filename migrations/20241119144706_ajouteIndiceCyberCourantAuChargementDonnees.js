d2e738b3423947b058d01aada652f4875fde8bb6
Christophe Cadilhac <christophe@malea.dev>
2024-11-19 15:49:35 +0100

    Appelle le chargement de l'indice cyber courant

---
+++ migrations/20241119144706_ajouteIndiceCyberCourantAuChargementDonnees.js
@@ -0,0 +1,24 @@
+exports.up = knex => knex.raw(`
+
+CREATE OR REPLACE PROCEDURE journal_mss.charge_donnees()
+LANGUAGE SQL
+AS $$
+
+  CALL journal_mss.charge_donnees_completude();
+  CALL journal_mss.charge_donnees_description_service();
+  CALL journal_mss.charge_donnees_collaboratif_service();
+  CALL journal_mss.charge_donnees_type_service();
+  CALL journal_mss.charge_donnees_fonctionnalite_service();
+  CALL journal_mss.charge_donnees_caractere_personnel_service();
+  CALL journal_mss.charge_donnees_statuts_des_mesures();
+  CALL journal_mss.charge_donnees_risques();
+  CALL journal_mss.charge_donnees_indice_cyber_courant();
+  
+      
+  INSERT INTO journal_mss.technique_chargement_donnees(date_chargement) VALUES (now());
+      
+$$;
+
+`);
+
+exports.down = knex => knex.raw(`DROP PROCEDURE IF EXISTS journal_mss.charge_donnees();`)
