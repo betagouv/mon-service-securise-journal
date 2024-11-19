bfa58f37edd64b2767c9211cc75f8978cb821286
Christophe Cadilhac <christophe@malea.dev>
2024-11-19 15:49:24 +0100

    Crée la procédure qui charge l'indice cyber courant de chaque service

---
+++ migrations/20241119143802_creationProcedureStockeeChargeIndiceCyberCourant.js
@@ -0,0 +1,27 @@
+exports.up = knex => knex.raw(`
+
+CREATE OR REPLACE PROCEDURE journal_mss.charge_donnees_indice_cyber_courant()
+LANGUAGE SQL
+AS $$
+      
+    TRUNCATE TABLE journal_mss.donnees_indice_cyber_courant;
+        
+    INSERT INTO journal_mss.donnees_indice_cyber_courant (id_service, categorie, indice, date) 
+    SELECT DISTINCT ON (donnees ->> 'idService', details.categorie) 
+        donnees->>'idService' as id_service,
+        details."categorie",
+        details."indice",
+        date
+    FROM journal_mss.vue_evenements_sans_services_supprimes,
+         jsonb_to_recordset(donnees -> 'detailIndiceCyber') as details("categorie" text, "indice" float)
+    WHERE type = 'COMPLETUDE_SERVICE_MODIFIEE'
+    ORDER BY donnees ->> 'idService', details.categorie, date DESC;
+
+$$;
+
+`);
+
+exports.down = knex => knex.raw(`
+    DROP PROCEDURE IF EXISTS journal_mss.charge_donnees_indice_cyber_courant();
+`)
+
