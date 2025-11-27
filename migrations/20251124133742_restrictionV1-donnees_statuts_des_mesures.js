9ae4866f83f10034863dddaf870b5cd7d78e0ce1
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-11-27 16:33:32 +0100

    Limite aux services v1 les données qui alimentent `donnees_statuts_des_mesures`
    Pour les statuts de mesures, on veut séparer les services v1 des services v2 car les mesures sont très différentes.
    Donc ici, on limite aux services v1, et on créera plus tard une table pour les services v2.

---
+++ migrations/20251124133742_restrictionV1-donnees_statuts_des_mesures.js
@@ -0,0 +1,39 @@
+exports.up = knex => knex.raw(`
+
+CREATE OR REPLACE PROCEDURE journal_mss.charge_donnees_statuts_des_mesures()
+LANGUAGE SQL
+AS $$
+
+    TRUNCATE TABLE journal_mss.donnees_statuts_des_mesures;
+    
+    INSERT INTO journal_mss.donnees_statuts_des_mesures (
+                                                      id_service,
+                                                      id_mesure,
+                                                      statut,
+                                                      date)
+        WITH tous_les_evenements AS (SELECT donnees ->> 'idService' AS id_service,
+                                            details."idMesure"      AS id_mesure,
+                                            details."statut",
+                                            date
+                                    FROM journal_mss.evenements e,
+                                        jsonb_to_recordset(donnees -> 'detailMesures') AS details("idMesure" text,
+                                                                                                  "statut" text)
+                                    WHERE type = 'COMPLETUDE_SERVICE_MODIFIEE'
+                                    AND e.donnees ->> 'idService' NOT IN
+                                        (select donnees ->> 'idService' from journal_mss.evenements where type = 'SERVICE_SUPPRIME')
+                                    AND (e.donnees->>'versionService' IS NULL OR e.donnees->>'versionService' = 'v1')                                    
+                                )
+        SELECT DISTINCT
+          id_service,
+          id_mesure,
+          statut,
+          first_value(date) over par_service_mesure_statut as date
+        FROM tous_les_evenements
+        WINDOW par_service_mesure_statut AS (partition by id_service, id_mesure, statut order by date);
+
+$$;
+
+`);
+
+exports.down = knex => knex.raw(`DROP PROCEDURE IF EXISTS journal_mss.charge_donnees_statuts_des_mesures();`)
+
