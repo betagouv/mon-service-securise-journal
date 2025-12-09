5c212f02dce25eee4e1125ca7ecfa69ad8d3735b
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-12-09 16:07:16 +0100

    Crée les procédures stockées après les migrations
    Conceptuellement, la création des procédures stockées est une étape des migrations. Donc, on se branche sur l'abstraction existante.

--- scripts/migrations.sh
+++ scripts/migrations.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -e
 
+# Migrations KNEX
 npm ci
 ./node_modules/.bin/knex migrate:latest
+
+# Création de toutes les procédures stockées
+./procedures_stockees/cree_toutes_les_procedures.sh
