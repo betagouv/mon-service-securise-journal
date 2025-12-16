af80e08be05c30189e82df42b1ff9f7ddb2fc619
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-12-16 11:28:14 +0100

    Utilise seulement les lignes de complétude dont la version correspond à l'actuelle du service
    Avant ce commit, on gérait mal un service `v1` migré en `v2` : en effet, les lignes `v1`
    continuaient de peupler la table `donnees_statuts_des_mesures` alors qu'on souhaite
    seulement les lignes `v2` si le service a été migré.
    
    Avec ce commit, on retient les `v1` si le service est `v1`, les `v2` si le service
    a été créé en `v2` ou bien migré depuis du `v1`.

--- procedures_stockees/charge_donnees_statuts_des_mesures.sql
+++ procedures_stockees/charge_donnees_statuts_des_mesures.sql
@@ -5,28 +5,45 @@ $$
 
 TRUNCATE TABLE journal_mss.donnees_statuts_des_mesures;
 
-INSERT INTO journal_mss.donnees_statuts_des_mesures (
-    id_service,
-    id_mesure,
-    statut,
-    date)
-WITH tous_les_evenements AS (
-    SELECT
-        donnees ->> 'idService' AS id_service,
-        details."idMesure"      AS id_mesure,
-        details."statut",
-        date
-    FROM journal_mss.evenements,
-      jsonb_to_recordset(donnees -> 'detailMesures') AS details("idMesure" text, "statut" text)
+INSERT INTO journal_mss.donnees_statuts_des_mesures (id_service, version_service, id_mesure, statut, date)
+WITH versions_actuelles_des_services AS (
+    SELECT DISTINCT
+        donnees ->> 'idService' as id_service,
+        first_value(donnees ->> 'versionService') over par_service as version_service
+    FROM journal_mss.evenements e
     WHERE type = 'COMPLETUDE_SERVICE_MODIFIEE'
-    AND (donnees->>'versionService' IS NULL OR donnees->>'versionService' = 'v1')
+    WINDOW par_service AS (partition by e.donnees ->> 'idService' order by date desc)
+),
+-- On veut seulement garder les lignes correspondant à la version actuelle :
+-- on écarte les éventuelles lignes "pré-migration".
+les_completudes_de_la_version_actuelle AS (
+    SELECT
+        actuelle.id_service,
+        actuelle.version_service as version_actuelle,
+        donnees -> 'detailMesures' as details_mesures,
+        e.date
+    FROM journal_mss.evenements e
+             JOIN versions_actuelles_des_services actuelle on actuelle.id_service = e.donnees->>'idService'
+    WHERE type = 'COMPLETUDE_SERVICE_MODIFIEE'
+      AND actuelle.version_service = donnees ->> 'versionService'
+),
+mesures_mises_a_plat AS (
+    SELECT
+        id_service,
+        version_actuelle,
+        details."idMesure" as id_mesure,
+        details.statut,
+        date
+    FROM les_completudes_de_la_version_actuelle,
+         jsonb_to_recordset(details_mesures) AS details("idMesure" text, "statut" text)
 )
 SELECT DISTINCT
     id_service,
+    version_actuelle,
     id_mesure,
     statut,
     first_value(date) over par_service_mesure_statut as date
-FROM tous_les_evenements
+FROM mesures_mises_a_plat
 WINDOW par_service_mesure_statut AS (partition by id_service, id_mesure, statut order by date);
 
 $$;
